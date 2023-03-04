284b95d635d8f143d5e0a83fd16fc2eb8d8a99c0
Apollo2113 <petrenkoroman1@gmail.com>
2023-03-04 23:29:18 +0200

    added constructor

--- src/Employee.java
+++ src/Employee.java
@@ -7,4 +7,15 @@ public class Employee {
     private String email;
     private String phone;
     private String age;
+
+    public Employee(String name, String lastName, String surName, String position,
+                    String email, String phone, String age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.surName = surName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
 }
