ab43d73319572a12a5beaca115ee3e343918a228
Apollo2113 <petrenkoroman1@gmail.com>
2023-03-04 23:26:41 +0200

    added new class Employee

---
+++ src/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+
+    private String name;
+    private String lastName;
+    private String surName;
+    private String position;
+    private String email;
+    private String phone;
+    private String age;
+}
